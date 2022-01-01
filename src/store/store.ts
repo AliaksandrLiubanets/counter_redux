737918caf1ed75638eb740e569853e3f37ed1780
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-01-01 21:34:04 +0300

    type AppStateType in store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import {combineReducers, createStore} from 'redux'
+import {stateReducer} from './stateReducer'
 
 const rootReducer = combineReducers({
     state: stateReducer
 })
 
 export const store = createStore(rootReducer)
+
+export type AppStateType = ReturnType<typeof rootReducer>
