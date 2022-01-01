07cc852b0e0dc4124ffe5f2ec81e7a4fd6b7f897
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-01-01 21:19:39 +0300

    add SET_SECOND_VARIANT as constant to type of SetSecondVariantType

--- src/store/stateReducer.ts
+++ src/store/stateReducer.ts
@@ -1,6 +1,7 @@
 const SET_MAX_VALUE = 'counter/state-reducer/SET_MAX_VALUE'
 const SET_START_VALUE = 'counter/state-reducer/SET_START_VALUE'
 const SET_EDIT_MODE = 'counter/state-reducer/SET_EDIT_MODE'
+const SET_SECOND_VARIANT = 'counter/state-reducer/SET_SECOND_VARIANT'
 
 const initialState = {
   maxValue: 0,
@@ -26,7 +27,12 @@ type SetEditModeType = {
   editMode: boolean
 }
 
-type ActionType = SetMaxValueType | SetStartValueType
+type SetSecondVariantType = {
+  type: typeof SET_SECOND_VARIANT
+  isSecondVariant: boolean
+}
+
+type ActionType = SetMaxValueType | SetStartValueType | SetEditModeType | SetSecondVariantType
 
 export const stateReducer = (state: StateType = initialState, action: ActionType): StateType => {
   switch (action.type) {
