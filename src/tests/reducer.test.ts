7a86644ae6e1f140450f4813af06c5c05511bcd0
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-01-03 21:57:07 +0300

    create test reducer.test.ts

--- src/tests/reducer.test.ts
+++ src/tests/reducer.test.ts
@@ -14,7 +14,9 @@ beforeEach(() => {
 test('set start value', () => {
 
     const result = stateReducer(state,{type: StateValues.SET_START_VALUE, startValue: 1})
+    const result2 = stateReducer(state,{type: StateValues.SET_START_VALUE, startValue: -1})
 
     expect(result.startValue).toBe(1)
+    expect(result2.startValue).toBe(-1)
 })
 
